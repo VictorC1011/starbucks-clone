1d190ed8bb209df037fdb2946df08302119f4afc
Victor <105716896+VictorC1011@users.noreply.github.com>
2022-11-10 04:21:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function btnSignin() {window.location.href="https://www.starbucks.com/account/signin?ReturnUrl=%2F"}
+function btnJoin() {window.location.href="https://www.starbucks.com/account/create"}
+function btnFindstore() {window.location.href="https://www.starbucks.com/store-locator"}
